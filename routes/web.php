dc900df9a9c7d0d7ae3919c4febc81bdeed7007d
Prity Islam <momotaj283107@gmail.com>
2019-09-09 12:04:29 +0600

    Add new messages

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+require 'admin.php';
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+
+// Route::view('/admin', 'admin.dashboard.index');
+//ui-element//
+// Route::view('/bootstrap-components', 'admin.dashboard.bootstrap-components');
+// Route::view('/widgets', 'admin.dashboard.widgets');
+// Route::view('/ui-cards', 'admin.dashboard.ui-cards');
+// //from//
+// Route::view('/form-components', 'admin.dashboard.form-components');
+// Route::view('/form-custom', 'admin.dashboard.form-custom');
+// Route::view('/form-samples', 'admin.dashboard.form-samples');
+// Route::view('/form-notifications', 'admin.dashboard.form-notifications');
+// //table//
+// Route::view('/table-data-table', 'admin.dashboard.table-data-table');
+// Route::view('/table-basic', 'admin.dashboard.table-basic');
+// //chart//
+// Route::view('/charts', 'admin.dashboard.charts');
+// ///page//
+// Route::view('/page-mailbox', 'admin.dashboard.page-mailbox');
+// Route::view('/blank-pase', 'admin.dashboard.blank-pase');
+// Route::view('/page-lockscreen', 'admin.dashboard.page-lockscreen');
+// Route::view('/page-invoice', 'admin.dashboard.page-invoice');
+// Route::view('/page-error', 'admin.dashboard.page-error');
+// Route::view('/page-login', 'admin.dashboard.page-login');
+// Route::view('/page-calendar', 'admin.dashboard.page-calendar');
+// Route::view('/page-user', 'admin.dashboard.page-user');
+
+
